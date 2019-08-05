ec2369b757ee7fa6a27ce7bec2d6751b8b7360dc
MirkoPieri <mirko97.pieri@gmail.com>
2019-08-05 15:19:58 +0200

    Piccoli aggiustamenti e inizio Milestone 2.

--- script.js
+++ script.js
@@ -24,13 +24,20 @@ $("header button").click(function() {
         // handlebars per stampare su schermo i risultati
         var source   = document.getElementById("template").innerHTML;
         var template = Handlebars.compile(source);
-
-        // ciclo per prendere tutti gli elementi restituiti dalla chiamata ajax
-        for (var i = 0; i < lunghezza; i++) {
-          var context = {titolo: arrayRisultati[i].title, originalTitle: arrayRisultati[i].original_title, lingua: arrayRisultati[i].original_language, voto: arrayRisultati[i].vote_average};
-          var html = template(context);
-          $(".general").append(html);
-        }
+          // ciclo per prendere tutti gli elementi restituiti dalla chiamata ajax
+          for (var i = 0; i < lunghezza; i++) {
+            // modifica del voto ad una scala da 1 a 5 con arrotondamento per eccesso
+            var stella = arrayRisultati[i].vote_average / 2;
+            var votoStella = Math.round(stella);
+            // condizione se il film è senza classificazione
+            if (votoStella === 0) {
+              votoStella = "Non Classificato";
+            }
+            // stampa dei risultati a schermo
+            var context = {titolo: arrayRisultati[i].title, originalTitle: arrayRisultati[i].original_title, lingua: arrayRisultati[i].original_language, voto: votoStella};
+            var html = template(context);
+            $(".general").append(html);
+          }
 
       },
       error: function (richiesta,stato,errore) {
