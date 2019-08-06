99e81da564ab965c70b3fdfb2e32644b1bdbab50
MirkoPieri <mirko97.pieri@gmail.com>
2019-08-06 15:31:27 +0200

    Aggiunta poster e inizio layout

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 $(document).ready(function() {
 
 
-$("header button").click(function() {
+// $("header button").click(myfunction);
+
+$("header input").keyup(function() {
+  if (event.which == 13) {
+    myfunction();
+  }
+});
+
+  function myfunction() {
 
   var ricercaUtente = $("header input").val();
   console.log(ricercaUtente);
   // pulizia dell'html ad ogni ricerca
   $(".general").html("");
 
+
   // chiamata ajax
+  // chiamata per film
   $.ajax (
     {
       url: "https://api.themoviedb.org/3/search/movie?api_key=7281fa587a1cb4960ded5c5f97ee3e8d",
       data: {"query": ricercaUtente}, //query per ricerca film digitato
       method: "GET",
       success: function(data) {
+
         var risultato = data;
         console.log(risultato);
         var arrayRisultati = risultato.results;
@@ -35,11 +46,13 @@ $("header button").click(function() {
             if (votoStella === 0) {
               votoStella = "Non Classificato";
             }
+
+            var immagine = arrayRisultati[i].poster_path;
+            console.log(immagine, "immagine");
             // stampa dei risultati a schermo
-            var context = {titolo: arrayRisultati[i].title, originalTitle: arrayRisultati[i].original_title, lingua: arrayRisultati[i].original_language, voto: votoStella};
+            var context = {titolo: arrayRisultati[i].title, originalTitle: arrayRisultati[i].original_title, lingua: arrayRisultati[i].original_language, voto: votoStella, tipo: "Film", url: 'https://image.tmdb.org/t/p/w300' + immagine};
             var html = template(context);
             $(".general").append(html);
-            console.log(votoStella, "questo voto stella");
 
             // funzione per sostituire le varie valutazioni con le rispettive stelle
             $(".film .valutazione").each(function() {
@@ -91,7 +104,97 @@ $("header button").click(function() {
       }
   }
 )
-});
+
+
+
+// chiamata ajax
+// chiamata serie tv
+$.ajax (
+  {
+    url: "https://api.themoviedb.org/3/search/tv?api_key=7281fa587a1cb4960ded5c5f97ee3e8d",
+    data: {"query": ricercaUtente}, //query per ricerca film digitato
+    method: "GET",
+    success: function(data) {
+
+      var risultatoTv = data;
+      console.log(risultatoTv);
+      var arrayRisultatiTv = risultatoTv.results;
+      var lunghezzaTv = arrayRisultatiTv.length;
+      console.log(lunghezzaTv);
+
+      // handlebars per stampare su schermo i risultati
+      var source   = document.getElementById("template").innerHTML;
+      var template = Handlebars.compile(source);
+        // ciclo per prendere tutti gli elementi restituiti dalla chiamata ajax
+        for (var i = 0; i < lunghezzaTv; i++) {
+          // modifica del voto ad una scala da 1 a 5 con arrotondamento per eccesso
+          var stella = arrayRisultatiTv[i].vote_average / 2;
+          var votoStella = Math.round(stella);
+          // variabile per salvare la lingua
+          var language = arrayRisultatiTv[i].original_language;
+          // condizione se il film è senza classificazione
+          if (votoStella === 0) {
+            votoStella = "Non Classificato";
+          }
+          // variabile per salvare url immagine
+          var immagine = 'https://image.tmdb.org/t/p/w300' + arrayRisultatiTv[i].poster_path;
+          // stampa dei risultati a schermo
+          var context = {titolo: arrayRisultatiTv[i].name, originalTitle: arrayRisultatiTv[i].original_name, lingua: arrayRisultatiTv[i].original_language, voto: votoStella, tipo: 'Tv', url: immagine};
+          var html = template(context);
+          $(".general").append(html);
+
+          // funzione per sostituire le varie valutazioni con le rispettive stelle
+          $(".film .valutazione").each(function() {
+            switch (true) {
+              case votoStella === 1:
+                $(this).replaceWith('<img src="image/star.png" alt="stella">');
+                break;
+              case votoStella === 2:
+                $(this).replaceWith('<img src="image/star.png" alt="stella">' + '<img src="image/star.png" alt="stella">');
+                break;
+              case votoStella === 3:
+                $(this).replaceWith('<img src="image/star.png" alt="stella">' + '<img src="image/star.png" alt="stella">' + '<img src="image/star.png" alt="stella">');
+                break;
+              case votoStella === 4:
+              $(this).replaceWith('<img src="image/star.png" alt="stella">' + '<img src="image/star.png" alt="stella">' + '<img src="image/star.png" alt="stella">' + '<img src="image/star.png" alt="stella">');
+                break;
+              case votoStella === 5:
+              $(this).replaceWith('<img src="image/star.png" alt="stella">' + '<img src="image/star.png" alt="stella">' + '<img src="image/star.png" alt="stella">' + '<img src="image/star.png" alt="stella">' + '<img src="image/star.png" alt="stella">');
+                break;
+              default:
+            }
+          });
+
+          // funzione per sostituire nome lingua film con bandiera corrispondente
+          $(".film .lingua").each(function() {
+            switch (true) {
+              case language === "en":
+                $(this).replaceWith('<img src="image/en.png" alt="lingua">');
+                break;
+              case language === "it":
+                $(this).replaceWith('<img src="image/it.png" alt="lingua">');
+                break;
+              case language === "et":
+                $(this).replaceWith('<img src="image/et.png" alt="lingua">');
+                break;
+              case language === "pt":
+              $(this).replaceWith('<img src="image/pt.png" alt="lingua">');
+                break;
+                case language === "":
+                $(this).replaceWith('Lingua non disponibile');
+                  break;
+              default:
+            }
+          });
+        }
+    },
+    error: function (richiesta,stato,errore) {
+      alert("problema sul server", errore);
+    }
+}
+
+)
+}
 
 
 });
